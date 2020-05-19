b1d3721f53be461f5280e34cf00c810a6d96db7c
mng <plvtomania@criptext.com>
2020-05-19 23:19:46 +0800

    added defanging an ip address

--- 2_defanging_an_ip_address.py
+++ 2_defanging_an_ip_address.py
@@ -1,2 +1,5 @@
-# references
-# https://leetcode.com/problems/defanging-an-ip-address/
+class Solution:
+    def defangIPaddr(self, address: str) -> str:
+        return ('[.]'.join((str(i) for i in address.split('.'))))
+        
+        
