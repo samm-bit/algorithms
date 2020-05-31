3ab40554db23bc7a960d323577546b9de1ab2cf4
mng <plvtomania@criptext.com>
2020-05-31 19:00:57 +0800

    change folder structure

--- leetcode/2_defanging_an_ip_address.py
+++ leetcode/2_defanging_an_ip_address.py
@@ -1,3 +1,7 @@
+# references
+# https://leetcode.com/problems/defanging-an-ip-address/
+
+
 class Solution:
     def defangIPaddr(self, address: str) -> str:
         return ('[.]'.join((str(i) for i in address.split('.'))))
